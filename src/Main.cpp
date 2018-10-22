63c46a134ae35d2eb59fb4a3a41e1e66eff5a882
Joona Rissanen <joona.m.rissanen@gmail.com>
2018-10-22 21:13:37 +0300

    Main renamed

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,10 +2,8 @@
 
 #include "Common.h"
 #include "Image.h"
-#include "Ray.h"
 #include "Hitable.h"
 #include "Camera.h"
-#include "Random.h"
 #include "Scene.h"
 #include "Worlds.h"
 #include "Render.h"
@@ -15,24 +13,25 @@ int main() {
     constexpr int factor {4};
     constexpr int width {factor * 100};
     constexpr int height {factor * 100};
-    constexpr int num_samples {200};
+    constexpr int num_samples {1000};
     constexpr int num_frames {1};
-    const vec3 lookfrom {30, 0, 0};
-    const vec3 lookat {0, 0, 0};
-    const vec3 vup {0, 1, 1};
-    const Float fov {36};
+    const vec3 look_from {30, 0, 0};
+    const vec3 look_at {0, 0, 0};
+    const vec3 view_up {0, 1, 1};
+    const Float vertical_fov {36};
     const Float aspect = Float(width)/Float(height);
     const Float aperture = 0.001;
     const Float focal_length = 10;
-    const Camera cam(lookfrom, lookat, vup, fov, aspect, aperture, focal_length);
+    const Camera cam(look_from, look_at, view_up, vertical_fov, aspect, aperture, focal_length);
     for (int i = 0; i < num_frames; i++) {
         Image im {width, height};
         Scene scene;
-        auto world = Worlds::atom_animation(scene, Float(i)/num_frames);
+        Float t = Float(i) / num_frames;
+        auto world = Worlds::atom_animation(scene, t);
         async_render(world, cam, num_samples, im);
-        im.gamma_correct();
         std::stringstream sst;
-        sst << "anim/a" << i << ".png";
+        sst << "anim/frame" << i << ".png";
+        im.gamma_correct();
         im.to_png(sst.str());
     }
     return 0;
